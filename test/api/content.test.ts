011cc262542f633f0802d74279cf7863c8660171
raimonfuns <raimonfuns@163.com>
2022-07-08 20:25:21 +0800

    remove some unused test cases

--- test/api/content.test.ts
+++
@@ -1,9 +0,0 @@
-import * as ContentApi from '../../src/api/content';
-
-describe('content api', () => {
-  it('list contents', async () => {
-    const groupId = '1d3996d4-943d-421d-898d-59df3dc64984';
-    const ret = await ContentApi.list(groupId);
-    console.log({ ret });
-  });
-});
